ed02227c0a2ab824da896658a72517cb58f2d2d3
pnamani <pnamani@allianceglobalservices.com>
2016-03-09 18:30:17 +0530

    Test 5

--- VSTestHook1/VSTestHook1/Global.asax.cs
+++ VSTestHook1/VSTestHook1/Global.asax.cs
@@ -15,7 +15,6 @@ namespace VSTestHook1
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles); 
-
         }
     }
 }
